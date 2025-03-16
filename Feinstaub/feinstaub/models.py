d36b0e3c4e51faacbba672330b92bb640aafd27f
Lukas Luemers <lukas.luemers@gdata.de>
2025-03-16 19:34:58 +0100

    added SDS011Metric Entity to DB

---
+++ Feinstaub/feinstaub/models.py
@@ -0,0 +1,24 @@
+from django.db import models;
+
+class SDS011Metric(models.Model):
+    id = models.AutoField(primary_key=True)  # Automatische Primärschlüssel-ID
+    sensor_id = models.IntegerField()  # ID des Sensors
+    sensor_type = models.CharField(max_length=50)  # Typ des Sensors (SDS011)
+    location = models.CharField(max_length=255, blank=True, null=True)  # Optionaler Standortname
+    lat = models.FloatField()  # Breitengrad
+    lon = models.FloatField()  # Längengrad
+    timestamp = models.DateTimeField(auto_now_add=True)  # Zeitstempel der Messung
+
+    # Feinstaubwerte
+    P1 = models.FloatField()  # Feinstaub PM10
+    durP1 = models.FloatField()  # Dauer PM10
+    ratioP1 = models.FloatField()  # Verhältnis PM10
+    P2 = models.FloatField()  # Feinstaub PM2.5
+    durP2 = models.FloatField()  # Dauer PM2.5
+    ratioP2 = models.FloatField()  # Verhältnis PM2.5
+
+    class Meta:
+        db_table = "sds011_metric"  # Setzt den Tabellennamen explizit auf 'sds011_metric'
+    
+    def __str__(self):
+        return f"SDS011 Sensor {self.sensor_id} - {self.timestamp}"
